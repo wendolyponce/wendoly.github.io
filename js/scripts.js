663e32f867d724d3391fcc612112b948cb790bfa
Wendoly Ponce <50433159+wendolyponce@users.noreply.github.com>
2024-06-05 14:04:54 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
+document.getElementById('copyEmailBtn').addEventListener('click', copyEmail);
+document.getElementById('copyEmailBtnContact').addEventListener('click', copyEmail);
+
 function sendEmail() {
     window.location.href = 'mailto:' + document.getElementById('email').innerText;
 }
 
 function copyEmail() {
     const email = document.getElementById('email').innerText;
-    navigator.clipboard.writeText(email).then(() => {
-        alert('Correo copiado al portapapeles');
-    });
+    navigator.clipboard.writeText(email);
 }
