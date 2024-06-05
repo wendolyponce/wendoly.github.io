a625e9ad4c3ea903c0c623be2ca060a8043f5995
Wendoly Ponce <50433159+wendolyponce@users.noreply.github.com>
2024-06-05 14:15:49 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
-document.getElementById('copyEmailBtn').addEventListener('click', copyEmail);
-document.getElementById('copyEmailBtnContact').addEventListener('click', copyEmail);
+document.getElementById('copyEmailIcon').addEventListener('click', function() {
+    copyEmail('email', 'copyEmailIcon', 'tooltipText');
+});
+document.getElementById('copyEmailIconContact').addEventListener('click', function() {
+    copyEmail('email-contact', 'copyEmailIconContact', 'tooltipTextContact');
+});
 
 function sendEmail() {
     window.location.href = 'mailto:' + document.getElementById('email').innerText;
 }
 
-function copyEmail() {
-    const email = document.getElementById('email').innerText;
-    navigator.clipboard.writeText(email);
+function copyEmail(emailId, iconId, tooltipId) {
+    const email = document.getElementById(emailId).innerText;
+    navigator.clipboard.writeText(email).then(() => {
+        const icon = document.getElementById(iconId);
+        const tooltip = document.getElementById(tooltipId);
+        icon.src = 'img/icons/check.png';
+        tooltip.innerText = 'Copiado!';
+        
+        setTimeout(() => {
+            icon.src = 'img/icons/copy.png';
+            tooltip.innerText = 'Copiar correo';
+        }, 2000);
+    });
 }
